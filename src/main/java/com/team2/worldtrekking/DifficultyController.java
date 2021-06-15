d0c00c7f8a589848689997cdbf3f75f2581c5515
Kouric-Calhoun <kouriccalhoun@gmail.com>
2021-06-15 14:36:36 -0400

    added controller for single page difficulty

--- src/main/java/com/team2/worldtrekking/DifficultyController.java
+++ src/main/java/com/team2/worldtrekking/DifficultyController.java
@@ -2,9 +2,11 @@ package com.team2.worldtrekking;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.annotation.Resource;
+import java.util.Optional;
 
 @Controller
 public class DifficultyController {
@@ -15,8 +17,16 @@ public class DifficultyController {
     @RequestMapping("/difficulties")
     public String displayDifficulties(Model model){
         model.addAttribute("difficultiesModel", difficultyRepo.findAll());
-        return "return difficultyView";
+        return "difficultiesView";
 
+    }
+
+    @RequestMapping("/difficulty/{id}")
+    public String displaySingleDifficulty(@PathVariable long id, Model model){
+        Optional<Difficulty> retrievedDifficulty = difficultyRepo.findById(id);
+        Difficulty foundDifficulty = retrievedDifficulty.get();
+        model.addAttribute("difficultiesModel", foundDifficulty);
+        return "difficultyView";
     }
 
 
