3624cdbd49d717067945cd12fd99f41eda4dd768
Randy Lacey, Jr <randolphlacey@gmail.com>
2021-06-15 17:24:18 -0400

    made the mainpage/worldtemplate our home page.

---
+++ src/main/java/com/team2/worldtrekking/MainPageController.java
@@ -0,0 +1,14 @@
+package com.team2.worldtrekking;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainPageController {
+
+    @RequestMapping({"/mainpage", "/", ""})
+    public String mainPage(){
+        return "worldTemplate";
+    }
+}
