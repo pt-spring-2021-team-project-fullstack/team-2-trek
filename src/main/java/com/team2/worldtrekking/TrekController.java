77e4efa665988535dd552045d9841558833f6b93
YMcElroyWCCI <yuli.mcelroy@gmail.com>
2021-06-15 08:45:43 -0400

    adding global header page, change all treks and single trek display

--- src/main/java/com/team2/worldtrekking/TrekController.java
+++ src/main/java/com/team2/worldtrekking/TrekController.java
@@ -2,9 +2,12 @@ package com.team2.worldtrekking;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.annotation.Resource;
+import java.util.Optional;
 
 @Controller
 public class TrekController {
@@ -12,10 +15,18 @@ public class TrekController {
     @Resource
     private TrekRepository trekRepo;
 
-    @RequestMapping("/trek")
-    public String displayTreks(Model model){
+    @RequestMapping("/treks")
+    public String displayTreks(Model model) {
         model.addAttribute("trekModel", trekRepo.findAll());
-        return "trekView";
+        return "treksView";
     }
 
+    @RequestMapping("/trek{id}")
+    public String displaySingleTrek(@RequestParam(value="id") Long id, Model model) {
+        Optional<Trek> retrievedTrek = trekRepo.findById(id);
+        Trek foundTrek = retrievedTrek.get();
+        model.addAttribute("trekModel", foundTrek);
+        return "TrekView";
+
+    }
 }
--- src/main/java/com/team2/worldtrekking/TrekRepository.java
+++ src/main/java/com/team2/worldtrekking/TrekRepository.java
@@ -4,4 +4,5 @@ import com.team2.worldtrekking.Trek;
 import org.springframework.data.repository.CrudRepository;
 
 public interface TrekRepository extends CrudRepository<Trek, Long> {
+
 }
