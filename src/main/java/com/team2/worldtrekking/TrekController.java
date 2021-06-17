701a0ba39abc646ddd5cfba18c2493b870377605
KS <kitsidhu@gmail.com>
2021-06-17 19:42:27 -0400

    WIP

--- src/main/java/com/team2/worldtrekking/TrekController.java
+++ src/main/java/com/team2/worldtrekking/TrekController.java
@@ -2,9 +2,7 @@ package com.team2.worldtrekking;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import java.util.Optional;
@@ -14,6 +12,15 @@ public class TrekController {
 
     @Resource
     private TrekRepository trekRepo;
+
+    @Resource
+    private ContinentRepository continentRepo;
+
+    @Resource
+    private RegionRepository regionRepo;
+
+//    @Resource
+//    private difficultyRepository difficultyRepo;
 
     @RequestMapping("/treks")
     public String displayTreks(Model model) {
@@ -27,6 +34,72 @@ public class TrekController {
         Trek foundTrek = retrievedTrek.get();
         model.addAttribute("trekModel", foundTrek);
         return "TrekView";
-
     }
+
+    @PostMapping("/add-trek")
+//    public String addTrek(@RequestParam String title, String description, String continent, String region,
+//                          String difficulty) {
+    public String addTrek(@RequestParam String title, String description, String continent, String region) {
+        Continent continentToAdd;
+        Region regionToAdd;
+//        Difficulty difficultyToAdd;
+
+        Optional<Continent> continentToAddOpt = continentRepo.findByTitle(title);
+        Optional<Region> regionToAddOpt = regionRepo.findByTitle(title);
+//        Optional<Difficulty> difficultyToAddOpt = difficultyRepo.findByMaterialType();
+
+        if(continentToAddOpt.isEmpty()){
+//            continentToAdd = new Continent(title, imageUrl);
+            continentToAdd = new Continent(title);
+            continentRepo.save(continentToAdd);
+        } else {
+            continentToAdd = continentToAddOpt.get();
+        }
+
+        if(continentToAddOpt.isEmpty()){
+//            continentToAdd = new Continent(title, imageUrl);
+            continentToAdd = new Continent(title);
+            continentRepo.save(continentToAdd);
+        } else {
+            continentToAdd = continentToAddOpt.get();
+        }
+
+//        if(difficultyToAddOpt.isEmpty()){
+//            difficultyToAdd = new Difficulty(difficultyLevel);
+//            continentRepo.save(continentToAdd);
+//        } else {
+//            continentToAdd = continentToAddOpt.get();
+//        }
+
+        Optional<Trek> trekToAddOpt = trekRepo.findByTitle(title);
+
+        if(trekToAddOpt.isEmpty()){
+//            Trek trekToAdd = new Trek(title, difficulty, description, price, imageUrl, continentToAdd, regionToAdd);
+//            Trek trekToAdd = new Trek(title, description, price, imageUrl, continentToAdd, regionToAdd);
+            Trek trekToAdd = new Trek(title, description, continentToAdd, regionToAdd);
+            trekRepo.save(trekToAdd);
+        }
+        return "redirect:/paper-plates/";
+    }
+
+    @GetMapping("/continents/{title}")
+    public String displaySingleContinent(@PathVariable String title, Model model){
+        Optional<Continent> retrievedContinent = continentRepo.findByTitle(title);
+        model.addAttribute("continentModel", retrievedContinent.get());
+        return "continentView";
+    }
+
+    @GetMapping("/regions/{title}")
+    public String displaySingleRegion(@PathVariable String title, Model model){
+        Optional<Region> retrievedRegion = regionRepo.findByTitle(title);
+        model.addAttribute("regionModel", retrievedRegion.get());
+        return "regionView";
+    }
+
+//    @GetMapping("difficulties/{title}")
+//    public String displaySingleDifficulty(@PathVariable String title, Model model){
+//        Optional<Difficulty> retrievedDifficulty = difficultyRepo.findByTitle(title);
+//        model.addAttribute("difficultyModel", retrievedDifficulty.get());
+//        return "difficultyView";
+//    }
 }
--- src/main/java/com/team2/worldtrekking/TrekRepository.java
+++ src/main/java/com/team2/worldtrekking/TrekRepository.java
@@ -3,6 +3,8 @@ package com.team2.worldtrekking;
 import com.team2.worldtrekking.Trek;
 import org.springframework.data.repository.CrudRepository;
 
-public interface TrekRepository extends CrudRepository<Trek, Long> {
+import java.util.Optional;
 
+public interface TrekRepository extends CrudRepository<Trek, Long> {
+    Optional<Trek> findByTitle(String title);
 }
